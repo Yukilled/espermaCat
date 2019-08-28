1fadbe96d62b3babf1194fc3a6dc47673ffc9903
Daniel <dangon21@ucm.es>
2019-08-28 22:05:14 +0200

    CogerObj

---
+++ New Unity Project/Assets/Scripts/CogerObjetos.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CogerObjetos : MonoBehaviour
+{
+    //Cada booleana controla cada arma y si puede o no ser usada.
+    bool esperma = false, carne = false, bolaDePelo = false, fluido = false;
+
+    public void SetBoolEsperma(bool estado)
+    {
+        if (!carne && !bolaDePelo && !fluido)
+        {
+            esperma = estado;
+            Debug.Log("Esperma cogido");
+        }
+    }
+    public void SetBoolCarne(bool estado)
+    {
+        if (!esperma && !bolaDePelo && !fluido)
+        {
+            carne = estado;
+            Debug.Log("carne cogido");
+        }
+    }
+    public void SetBoolBolaDePelo(bool estado)
+    {
+        if (!esperma && !carne && !fluido)
+        {
+            bolaDePelo = estado;
+            Debug.Log("Bola cogido");
+        }
+    }
+    public void SetBoolFluido(bool estado)
+    {
+        if (!esperma && !carne && !bolaDePelo)
+        {
+            fluido = estado;
+            Debug.Log("Esperma cogido");
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Objetos.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objetos : MonoBehaviour
+{
+    CogerObjetos co;
+    bool cogerObjeto, objetoCogido;
+    private void Update()
+    {
+        //Si esta en el trigger para poder coger el objeto y pulsa la tecla de cogerlo
+
+        if (cogerObjeto && Input.GetKeyDown(KeyCode.E))
+        {
+            objetoCogido = true;
+        }
+        Debug.Log(cogerObjeto);
+        
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        
+        //Poner IU de coger Objeto
+
+        if (objetoCogido) //si el jugador ha cogido el objeto
+        {
+            Debug.Log("Pulsado E");
+            //quitar UI de coger objeto
+            co = other.GetComponent<CogerObjetos>();
+            switch (this.gameObject.tag)
+            {
+                case "Esperma": co.SetBoolEsperma(true); break;
+                case "Carne": co.SetBoolCarne(true); break;
+                case "BolaDePelo": co.SetBoolBolaDePelo(true); break;
+                case "Fluido": co.SetBoolFluido(true); break;
+            }
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        cogerObjeto = true;
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        cogerObjeto = false;
+        objetoCogido = false;
+        //quitar UI de objeto cogido
+    }
+}

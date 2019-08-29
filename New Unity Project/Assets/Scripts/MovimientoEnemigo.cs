52cf86d2f41c13174e6014a99d170e08113bb153
Daniel <dangon21@ucm.es>
2019-08-29 06:27:25 +0200

    Menu y comienzo del primer nivel(unico)

---
+++ New Unity Project/Assets/Scripts/MovimientoEnemigo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoEnemigo : MonoBehaviour
+{
+    public GameObject jugador;
+    public float speed;
+    public float distancia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(new Vector2(transform.position.x,transform.position.y),new Vector2(jugador.transform.position.x, jugador.transform.position.y)) < distancia)
+        {
+            transform.Translate(new Vector2(jugador.transform.position.x - transform.position.x, jugador.transform.position.y - transform.position.y).normalized*speed*Time.deltaTime);
+        }
+    }
+}
